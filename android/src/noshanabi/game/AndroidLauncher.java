e073ebba887124016d0495eaf88dbe62a0f119e0
Trương Ngọc Sơn <alwayssmile11a1@outlook.com>
2017-11-11 22:29:36 +0700

    finish google login. Upcoming, facebook login

--- android/src/noshanabi/game/AndroidLauncher.java
+++ android/src/noshanabi/game/AndroidLauncher.java
@@ -3,16 +3,19 @@ package noshanabi.game;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.badlogic.gdx.backends.android.AndroidFragmentApplication;
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -24,29 +27,83 @@ import com.google.firebase.auth.GoogleAuthProvider;
 
 import noshanabi.game.Extensions.FacebookPlayerServices;
 
-public class AndroidLauncher extends AndroidApplication implements FacebookPlayerServices, GoogleApiClient.OnConnectionFailedListener {
+public class AndroidLauncher extends FragmentActivity implements AndroidFragmentApplication.Callbacks, FacebookPlayerServices{
 
+	GoogleApiClient mGoogleApiClient;
 
 	private FirebaseAuth mAuth;
-	private GoogleSignInClient mGoogleSignInClient;
 	private static int RC_SIGN_IN = 1;
 
 	private String TAG = "GOOGLE LOGIN";
 
+	FirebaseAuth.AuthStateListener mAuthListener;
+
+	// 4. Create a Class that extends AndroidFragmentApplication which is the Fragment implementation for Libgdx.
+	public static class GameFragment extends AndroidFragmentApplication implements FacebookPlayerServices
+	{
+		AndroidLauncher androidLauncher;
+
+		public void setAndroidLauncher(AndroidLauncher androidLauncher)
+		{
+			this.androidLauncher = androidLauncher;
+		}
+
+		// 5. Add the initializeForView() code in the Fragment's onCreateView method.
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{  return initializeForView(new GameManager( this));   }
+
+		@Override
+		public void signIn() {
+			androidLauncher.signIn();
+		}
+
+		@Override
+		public void signOut() {
+			androidLauncher.signOut();
+		}
+
+	}
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new GameManager(this), config);
+//		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+//		initialize(new GameManager(this), config);
+
+		// 6. Finally, replace the AndroidLauncher activity content with the Libgdx Fragment.
+		GameFragment fragment = new GameFragment();
+		fragment.setAndroidLauncher(this);
+		FragmentTransaction trans = getSupportFragmentManager().beginTransaction();
+		trans.replace(android.R.id.content, fragment);
+		trans.commit();
 
 		mAuth = FirebaseAuth.getInstance();
 
+		mAuthListener = new FirebaseAuth.AuthStateListener() {
+			@Override
+			public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+				if(firebaseAuth.getCurrentUser()!=null)
+				{
+
+				}
+				else
+				{
+
+				}
+			}
+		};
 
 		GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
 				.requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
 
 		// Build a GoogleSignInClient with the options specified by gso.
-		mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+		mGoogleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+			@Override
+			public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+				Log.d(TAG, "Failed to connect");
+			}
+		}).addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
 
 
 
@@ -57,8 +114,7 @@ public class AndroidLauncher extends AndroidApplication implements FacebookPlaye
 	public void onStart() {
 		super.onStart();
 
-		// Check if user is signed in (non-null) and update UI accordingly.
-		FirebaseUser currentUser = mAuth.getCurrentUser();
+		mAuth.addAuthStateListener(mAuthListener);
 	}
 
 	@Override
@@ -67,10 +123,9 @@ public class AndroidLauncher extends AndroidApplication implements FacebookPlaye
 
 	}
 
+
 	@Override
-	public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-		Log.d(TAG,"Connection failed");
-	}
+	public void exit() {}
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -78,15 +133,16 @@ public class AndroidLauncher extends AndroidApplication implements FacebookPlaye
 
 		// Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
 		if (requestCode == RC_SIGN_IN) {
-			Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-			try {
+			GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+			if(result.isSuccess())
+			{
 				// Google Sign In was successful, authenticate with Firebase
-				GoogleSignInAccount account = task.getResult(ApiException.class);
+				GoogleSignInAccount account = result.getSignInAccount();
 				firebaseAuthWithGoogle(account);
-			} catch (ApiException e) {
+			} else {
 				// Google Sign In failed, update UI appropriately
-				Log.w(TAG, "Google sign in failed", e);
-				// ...
+				Log.d(TAG, "failed on sign in api");
+
 			}
 		}
 	}
@@ -116,7 +172,8 @@ public class AndroidLauncher extends AndroidApplication implements FacebookPlaye
 
 	@Override
 	public void signIn() {
-		Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+
+		Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
 		startActivityForResult(signInIntent, RC_SIGN_IN);
 	}
 
